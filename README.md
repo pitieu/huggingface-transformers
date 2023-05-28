76bd50ae698afab1fe053d3b4e792ee10cf4b54f
Marcio Marques Soares <marcio1988@hotmail.com>
2023-05-28 09:09:22 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Examples using huggingface transformers
+
+## Javascript
+
+List of available models for javascript
+https://huggingface.co/models?other=transformers.js&sort=modified
+
+### Run javascript
+
+`npm run dev`
+
+## Python
+Using transformers pipeline is easier and more condensed code but the problem is if they are not updated you'll end up with warnings or broken code.
+
+<img src="./assets/long_python" width="300" >
+
+Both perform the same but this is more condensed
+<img src="./assets/short_python" width="300" >
+
+List of available models for python
+https://huggingface.co/models?library=transformers&sort=downloads
---
+++ index.js
@@ -0,0 +1,56 @@
+import express from "express";
+import { pipeline } from "@xenova/transformers";
+// List of available models
+// https://huggingface.co/models?other=transformers.js&sort=modified
+
+const app = express();
+const port = 3000;
+
+app.get("/classify", async (req, res) => {
+  try {
+    let pipe = await pipeline(
+      "text-classification",
+      "Xenova/distilbert-base-uncased-finetuned-sst-2-english"
+    );
+    let out = await pipe("I love transformers!");
+    res.json(out);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.get("/tag", async (req, res) => {
+  try {
+    let pipe = await pipeline(
+      "image-to-text",
+      "Xenova/vit-gpt2-image-captioning" 
+    );
+    let out = await pipe(
+      "./assets/3.jpg"
+    );
+    res.json(out);
+  } catch (e) {
+    console.log(e);
+    res.json({});
+  }
+});
+
+app.get("/tag2", async (req, res) => {
+  try {
+    let pipe = await pipeline(
+      "zero-shot-classification"
+    );
+    let out = await pipe(
+      "./assets/3.jpg"
+    );
+    res.json(out);
+  } catch (e) {
+    console.log(e);
+    res.json({});
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
